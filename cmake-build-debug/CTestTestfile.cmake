7b51b9833ff4d58400efb0c415763979a62d2428
Jalil_Ramazan <j_ramazan@kbtu.kz>
2024-01-09 15:17:42 +0600

    Edited gitignore

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jalil/Desktop/SimpleSQL-like_DB
-# Build directory: /home/jalil/Desktop/SimpleSQL-like_DB/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("tests")
